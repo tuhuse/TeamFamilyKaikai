25ad9b2dd313c341e11915dc31643792c66f041b
STD\s20237042 <tuhuse>
2024-07-25 17:11:54 +0900

    カウントダウンファイルにしまった

---
+++ Assets/Script/CountDown.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountDown : MonoBehaviour
+{
+    [SerializeField] private GameObject _count1;
+    [SerializeField] private GameObject _count2;
+    [SerializeField] private GameObject _count3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Count1() {
+        _count1.SetActive(false);
+    }
+    public void Count2() {
+        _count2.SetActive(false);
+        _count1.SetActive(true);
+    }
+    public void Count3() {
+        _count3.SetActive(false);
+        _count2.SetActive(true);
+    }
+}
