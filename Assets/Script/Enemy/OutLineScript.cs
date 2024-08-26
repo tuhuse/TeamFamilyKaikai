563909cf283549c42e001934bd42b2da18b2a5b4
katoyuuya <20237029-katoyuuya@hcs.ac.jp>
2024-08-26 15:23:18 +0900

    DeathEffectの色反映

--- Assets/Script/Enemy/OutLineScript.cs
+++ Assets/Script/Enemy/OutLineScript.cs
@@ -13,6 +13,8 @@ public class OutLineScript : MonoBehaviour
     [SerializeField] private GameObject _cpu3DeathAnim;
     [SerializeField] private GameObject _playerDeathAnim;
     [SerializeField] private CommentScript _commentScript = default;
+    [SerializeField] private ParticleSystem _particle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,8 +31,38 @@ public class OutLineScript : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "CPU") 
         {
-            _commentScript.CommentChange("ここで" + collision.gameObject.name + "がだつらく！！！", false);
+            ParticleSystem.MainModule main = _particle.main;
+            if (collision.gameObject.layer == 12) {//緑
+                main.startColor = new Color(0.625f, 0.772f, 0.47f, 1f);
+                _particle.Play();
+            }
+            if (collision.gameObject.layer == 13) {//黄色
+                main.startColor = new Color(1f, 0.827f, 0.243f, 1f);
+                _particle.Play();
+            }
+            if (collision.gameObject.layer == 10) {//青
+                main.startColor = new Color(0.47f, 0.662f, 0.772f, 1f);
+                _particle.Play();
+            }
+            if (collision.gameObject.layer == 8) {//ピンク
+                main.startColor = new Color(0.96f, 0.713f, 0.839f, 1f);
+                _particle.Play();
+            }
+            if (collision.gameObject == _cpu1) {//CPUピンク
+                main.startColor = new Color(0.96f, 0.713f, 0.839f, 1f);
+                _particle.Play();
+            }
+            if (collision.gameObject == _cpu2) {//CPU青
+                main.startColor = new Color(0.47f, 0.662f, 0.772f, 1f);
+                _particle.Play();
+            }
+            if (collision.gameObject == _cpu3) {//CPU黄色
+                main.startColor = new Color(1f, 0.827f, 0.243f, 1f);
+                _particle.Play();
+            }
 
+
+            _commentScript.CommentChange("ここで" + collision.gameObject.name + "がだつらく！！！", false);
             if (collision.gameObject == _cpu1) {
                 _cpu1DeathAnim.SetActive(true);
             }
