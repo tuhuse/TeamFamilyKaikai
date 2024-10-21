c14905a74d0d612ba88044b759790bac8730e68b
STD\s20237042 <tuhuse>
2024-10-21 19:21:39 +0900

    wqqfewds

---
+++ Assets/Script/Mode/MasterMFrog.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterMFrog : MonoBehaviour
+{
+    [SerializeField] private GameObject _player;
+
+
+    private bool _isAlive = default;
+
+    [SerializeField] private float _distancePlayer;
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _jumpPower;
+    
+    private Rigidbody2D _rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _isAlive = true;
+        _distancePlayer = 30f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DistanceMonoBehaviour();
+    }
+    private void DistanceMonoBehaviour() {
+        if (_isAlive) {
+            _rb.velocity = new Vector2(_moveSpeed, _rb.velocity.y);
+        }
+    }
+}
